fbcf7daa5f602418e0027f9a8f31cebdfce551e0
Nicolas Stuhlfauth <17216829+nicostuhlfauth@users.noreply.github.com>
2025-02-15 08:05:14 +0100

    Add React Router

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App.jsx';
+
+import './index.css';
+import { TempPage } from './pages/TempPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/temppage" element={<TempPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
