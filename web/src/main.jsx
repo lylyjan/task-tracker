0f4c2f20c03c141ee06495eb5fb43f91ca3a3163
Nicolas Stuhlfauth <17216829+nicostuhlfauth@users.noreply.github.com>
2025-02-22 09:44:50 +0100

    Add MSW (#6)

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -4,15 +4,34 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App.jsx';
 
 import './index.css';
+import { DataFetchingDemoPage } from './pages/DataFetchingDemoPage.jsx';
 import { TempPage } from './pages/TempPage.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/temppage" element={<TempPage />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (import.meta.env === 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start({
+    onUnhandledRequest: 'bypass', // Allows non-mocked requests to pass through
+  });
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/temppage" element={<TempPage />} />
+          <Route path="/datafetching" element={<DataFetchingDemoPage />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>,
+  );
+});
